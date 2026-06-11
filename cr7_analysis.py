e1a40c6912e35eec39a51b9992fa84479d9c3739
Yaroslav <yari4ok666@gmail.com>
2026-06-11 15:37:12 +0300

    second commit

--- cr7_analysis.py
+++ cr7_analysis.py
@@ -16,26 +16,27 @@ print("Вдома:", df["Venue"].value_counts()["H"], "| На виїзді:", df
 print("Найкращий сезон:", df["Season"].value_counts().idxmax(), df["Season"].value_counts().max(), "голів")
 print("Пропуски:\n", df.isnull().sum())
 
+# """
+# Graphic — Goals/Score of teams
+# """
+# df["Team"].value_counts().plot(kind="bar", title="Goals of teams", color="steelblue")
+# plt.tight_layout()
+# plt.show()
+
+# """
+# Graphic — Top-10 Opponent
+# """
+# df["Opponent"].value_counts().head(10).plot(kind="bar", title="Top-10 Opponent", color="tomato")
+# plt.tight_layout()
+# plt.show()
+
 """
-Графік — голи по командах
+Graphic — Type_of_goal
 """
-df["Team"].value_counts().plot(kind="bar", title="Голи по командах", color="steelblue")
+df["Type_of_goal"].value_counts().plot(kind="barh", title="Type_of_goal", color="steelblue")
 plt.tight_layout()
 plt.show()
 
-"""
-Графік — топ-10 суперників
-"""
-df["Opponent"].value_counts().head(10).plot(kind="bar", title="Топ-10 суперників", color="tomato")
-plt.tight_layout()
-plt.show()
-
-"""
-Графік — тип голу
-"""
-df["Type_of_goal"].value_counts().plot(kind="barh", title="Тип голу", color="steelblue")
-plt.tight_layout()
-plt.show()
 
 print(
     """
